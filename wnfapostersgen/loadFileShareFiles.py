a4c775162cb49b6487a0a0b232d1c5a338c4fe57
Gengyuan Huang <gengyuan@ualberta.ca>
2022-07-10 20:23:27 -0600

    Start working on async client

--- wnfapostersgen/loadFileShareFiles.py
+++ wnfapostersgen/loadFileShareFiles.py
@@ -1,6 +1,8 @@
-import logging
 from azure.storage.fileshare import ShareClient
 from wnfapostersgen.secrets import connection_string
+from azure.storage.fileshare.aio import ShareClient as ShareClientAsync
+
+import asyncio
 
 share = ShareClient.from_connection_string(conn_str=connection_string, share_name="assets")
 
@@ -11,3 +13,34 @@ def list_all_files(path):
 def get_file_binary(path):
     file_client = share.get_file_client(path)
     return file_client.download_file().readall()    
+
+
+
+
+
+async def get_data_from_fileclient(file_client):
+    data = await file_client.download_file()
+    return await data.readall()
+
+
+async def get_list_of_files_binary_async(a_list_path):
+    
+    # client
+    client = ShareClient.from_connection_string(conn_str=connection_string, share_name="assets")
+
+    # download files
+    tasks = []
+    for path in a_list_path:
+        file_client = client.get_file_client(path)
+        task = asyncio.create_task(get_data_from_fileclient(file_client))
+        tasks.append(task)
+    
+    # get binary
+    files_binary = []
+    for task in tasks:
+        files_binary.append(await task)
+    
+    # close client
+    await client.close()
+
+    return files_binary
